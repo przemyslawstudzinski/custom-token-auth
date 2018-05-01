36b6f1a5100eb80bd13f636ac9833e10711c114f
Przemysław Studziński <przemyslawstudzinski1993@gmail.com>
2018-05-01 20:42:49 +0200

    Added Resource Controller

--- src/main/java/com/example/basic/token/auth/web/ResourceController.java
+++ src/main/java/com/example/basic/token/auth/web/ResourceController.java
@@ -1,9 +1,38 @@
 package com.example.basic.token.auth.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ResourceController {
+
+  @RequestMapping(value = "/create", method = RequestMethod.GET)
+  @ResponseStatus(HttpStatus.OK)
+  public String create() {
+    return "CREATED";
+  }
+
+  @RequestMapping(value = "/edit", method = RequestMethod.GET)
+  @ResponseStatus(HttpStatus.OK)
+  public String edit() {
+    return "EDITED";
+  }
+
+  @RequestMapping(value = "/view", method = RequestMethod.GET)
+  @ResponseStatus(HttpStatus.OK)
+  public String view() {
+    return "VIEW";
+  }
+
+  @RequestMapping(value = "/delete", method = RequestMethod.GET)
+  @ResponseStatus(HttpStatus.OK)
+  public String delete() {
+    return "DELETED";
+  }
 
 
 
